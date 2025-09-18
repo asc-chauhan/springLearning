8cdf4e2c3075fc32fd05fd30d3b5022a203c34a7
asc-chauhan <anshulasc151976@gmail.com>
2025-09-19 00:49:50 +0530

    First bean

---
+++ src/main/java/com/loose/coupling/UserDatabaseProvider.java
@@ -0,0 +1,11 @@
+package com.loose.coupling;
+
+// A - MYSQL, POSTGRESQL
+// B - Web Service, MongoDB
+
+public class UserDatabaseProvider implements UserDataProvider{
+    @Override
+    public String getUserDetails(){
+        return "User details From Database";
+    }
+}
---
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -0,0 +1,10 @@
+package com.tight.coupling;
+
+// A - MYSQL, POSTGRESQL
+// B - Web Service, MongoDB
+
+public class UserDatabase{
+    public String getUserDetails(){
+        return "User details From Database";
+    }
+}

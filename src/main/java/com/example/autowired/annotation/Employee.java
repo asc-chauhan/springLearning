eb5c19c73e520961d9a2b30d7c00fad9623ca779
asc-chauhan <anshulasc151976@gmail.com>
2025-09-19 22:15:57 +0530

    annotations

---
+++ src/main/java/com/example/autowired/annotation/Employee.java
@@ -0,0 +1,58 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//by default the class name comes to lowercase and then component is defined
+@Component("employee")
+public class Employee {
+    private Integer employeeId;
+    @Value("Hello")
+    private String firstName;
+    @Value("${java.home}")
+    private String lastName;
+    @Value("#{4*4}")
+    private double salary;
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -0,0 +1,26 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+    //this is field injection
+    @Autowired
+    @Qualifier("employee")
+    private Employee employee;
+
+//    this is constructor injection
+//    @Autowired
+//    public Manager(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
